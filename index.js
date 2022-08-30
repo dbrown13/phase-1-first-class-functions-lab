e73c8c1463ac996d43a9188d647647310fe6256b
Deryn Brown <Deryn.2017@gmail.com>
2022-08-29 23:26:08 -0500

    commit tree 695e7368b444684d960dec75b9b30d2e3ddd8c42

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier = 4) => {
+    return (fare) => {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (drivers, fn) => {
+    return fn(drivers);
+
+}
